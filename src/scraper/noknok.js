324476f68135045e171384d3fea53e50a9112f43
khudiakov <khudiakov@outlook.com>
2018-02-06 21:28:55 +0100

    fixed parser

--- src/scraper/noknok.js
+++ src/scraper/noknok.js
@@ -10,7 +10,7 @@ const { JSDOM } = jsdom;
 const RESTAURANT_ID = "5a7a0a6ebc410a19beb95fe1";
 
 const getWeekMenu = async () => {
-  const res = await fetch("https://www.noknokrestaurant.cz/");
+  const res = await fetch("http://www.noknokrestaurant.cz/");
   const page = await res.text();
   const dom = new JSDOM(page);
   return Array.from(
@@ -19,22 +19,26 @@ const getWeekMenu = async () => {
       .getElementsByTagName("li")
   )
     .filter(el => el.className.match(/erm_section|erm_product/) != null)
-    .reduce((acc, el) => el.className === "erm_section"
-        ? acc.push({
-            date: parseDate(
-              new Date().getFullYear(),
-              ...el.childNodes[0].textContent
-                .match(/(\d+)\. (\d+)/)
-                .slice(1, 3)
-                .map(v => parseInt(v))
-                .reverse()
-            ),
-            food: []
-          }) && acc
-        : acc[acc.length - 1]["food"].push({
-            name: el.getElementsByClassName("erm_product_desc")[0].textContent,
-            price: parseInt(el.getElementsByClassName("price")[0].textContent)
-          }) && acc
-    , [])
-
+    .reduce(
+      (acc, el) =>
+        el.className === "erm_section"
+          ? acc.push({
+              date: parseDate(
+                new Date().getFullYear(),
+                ...el.childNodes[0].textContent
+                  .match(/(\d+)\. (\d+)/)
+                  .slice(1, 3)
+                  .map(v => parseInt(v))
+                  .reverse()
+              ),
+              food: []
+            }) && acc
+          : acc[acc.length - 1]["food"].push({
+              name: el.getElementsByClassName("erm_product_desc")[0]
+                .textContent,
+              price: parseInt(el.getElementsByClassName("price")[0].textContent)
+            }) && acc,
+      []
+    );
+};
 export { RESTAURANT_ID, getWeekMenu };
